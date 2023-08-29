79aec3a49d212202bee6616bd8917a22ecff7971
Vi Prime <1724913+vprime@users.noreply.github.com>
2023-08-29 16:33:35 -0700

    Allow moving the viewer, and spawning around the viewer.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # stela-nubo
 Stela nubo is a game created with Bevy and Rust
+
+## Goals
+Spawn and despawn objects around the player deterministicly
+
+Move around the space in a spaceship that can collide with the objects
+Be able to shoot and explode things, and be exploded
+Things that move
+Add gravity to things
+Multiplayer
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use bevy::input::mouse::MouseMotion;
 // Entrypoint for the main game binary
 use bevy::prelude::*;
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.0, 0.0, 0.0);
 const SPIN_SPEED:Vec3 = Vec3::new(0.0, 0.0, 1.0);
-const MOVE_SPEED:Vec3 = Vec3::new(-0.9, -0.1, -0.1);
+const MOVE_SPEED:f32 = 0.9;
 const SPAWN_LIFETIME:f32 = 10.0;
 const SPAWN_SPEED:f32 = 0.0001;
 
@@ -17,12 +18,14 @@ fn main() {
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .insert_resource(SpawnTimer(Timer::from_seconds(SPAWN_SPEED, TimerMode::Repeating)))
         .add_systems(Startup, setup)
-        .add_systems(Update, (spin_cubes, move_cubes, spawn_cubes, despawn_cubes))
+        .add_systems(Update, (
+            spin_cubes,
+            move_viewer,
+            spawn_cubes,
+            despawn_cubes
+        ))
         .run();
 }
-
-#[derive(Component, Deref, DerefMut)]
-struct Velocity(Vec3);
 
 #[derive(Component, Deref, DerefMut)]
 struct Spin(Vec3);
@@ -30,19 +33,62 @@ struct Spin(Vec3);
 #[derive(Component, Deref)]
 struct Death(f32);
 
-fn move_cubes(
-    mut query: Query<(&mut Transform, &Velocity, &Death)>,
+#[derive(Component)]
+struct Viewer;
+
+
+fn move_viewer(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut mouse_input: EventReader<MouseMotion>,
+    mut query: Query<&mut Transform, With<Viewer>>,
     time: Res<Time>
 ){
     let delta = time.delta_seconds();
-    for(mut transform, velocity, death) in &mut query {
-        let deathMultiplyer = (death.0 - time.elapsed_seconds()) * 0.7;
-        transform.translation.x += velocity.x * delta * deathMultiplyer;
-        transform.translation.x += velocity.y * delta * deathMultiplyer;
-        transform.translation.x += velocity.z * delta * deathMultiplyer;
+    let mut viewer_transform = query.single_mut();
+    let mut direction = Vec3::ZERO;
+
+    if keyboard_input.pressed(KeyCode::A) {
+        direction += viewer_transform.left();
+    }
+    if keyboard_input.pressed(KeyCode::D) {
+        direction += viewer_transform.right();
+    }
+    if keyboard_input.pressed(KeyCode::W) {
+        direction += viewer_transform.forward();
+    }
+    if keyboard_input.pressed(KeyCode::S) {
+        direction += viewer_transform.back();
+    }
+    if keyboard_input.pressed(KeyCode::Space) {
+        direction += viewer_transform.up();
+    }
+    if keyboard_input.pressed(KeyCode::ShiftLeft){
+        direction += viewer_transform.down();
     }
 
+    viewer_transform.translation.x += MOVE_SPEED * direction.x * delta;
+    viewer_transform.translation.y += MOVE_SPEED * direction.y * delta;
+    viewer_transform.translation.z += MOVE_SPEED * direction.z * delta;
+
+    for movement in mouse_input.iter() {
+        viewer_transform.rotate_local_y(movement.delta.x * 0.005 * -1.0);
+        viewer_transform.rotate_local_x(movement.delta.y * 0.005);
+    }
 }
+
+// fn move_cubes(
+//     mut query: Query<(&mut Transform, &Death)>,
+//     time: Res<Time>
+// ){
+//     let delta = time.delta_seconds();
+//     for(mut transform, velocity, death) in &mut query {
+//         let deathMultiplyer = (death.0 - time.elapsed_seconds()) * 0.7;
+//         transform.translation.x += velocity.x * delta * deathMultiplyer;
+//         transform.translation.y += velocity.y * delta * deathMultiplyer;
+//         transform.translation.z += velocity.z * delta * deathMultiplyer;
+//     }
+//
+// }
 
 fn spin_cubes(
     mut query: Query<(&mut Transform, &Spin)>,
@@ -73,26 +119,30 @@ fn spawn_cubes(mut commands: Commands,
          time: Res<Time>,
          mut timer: ResMut<SpawnTimer>,
          mut meshes: ResMut<Assets<Mesh>>,
-         mut materials: ResMut<Assets<StandardMaterial>>) {
-
+         mut materials: ResMut<Assets<StandardMaterial>>,
+        query: Query<(&Transform, &Viewer)>) {
 
     // if !timer.0.tick(time.delta()).just_finished() {
     //     return;
     // }
-    let base_time: f32 = time.delta_seconds();
-    let set_x: f32 = (base_time * 10000.0 % 2.0) + 5.0;
-    let set_y: f32 = (base_time * 100000.0 % 6.0) - 3.0;
-    let set_z: f32 = (base_time * 1000000.0 % 2.0);
-    commands.spawn((PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube {size: 0.25})),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(set_x, set_y, set_z),
-        ..default()
-    },
-                    Velocity(MOVE_SPEED),
-                    Spin(SPIN_SPEED * set_z),
-        Death(time.elapsed_seconds() + SPAWN_LIFETIME)
-    ));
+    for (transform, viewer) in &query {
+        let base_time: f32 = time.delta_seconds();
+
+        let set_x: f32 = transform.translation.x + (base_time * 10000.0 % 6.0) - 3.0;
+        let set_y: f32 = transform.translation.y + (base_time * 1000000.0 % 6.0) - 3.0;
+        let set_z: f32 = transform.translation.z + (base_time * 100000000.0 % 6.0) - 3.0;
+        commands.spawn((PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube {size: 0.25})),
+            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+            transform: Transform::from_xyz(set_x, set_y, set_z),
+            ..default()
+        },
+            Spin(SPIN_SPEED),
+            Death(time.elapsed_seconds() + SPAWN_LIFETIME)
+        ));
+    }
+
+
 }
 
 
@@ -133,21 +183,25 @@ fn setup(
     //                 Velocity(SPIN_SPEED)
     // ));
 
-    // light
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..default()
-    });
 
     // camera
-    commands.spawn(Camera3dBundle {
+    commands.spawn((Camera3dBundle {
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
-    });
+    },
+        Viewer,
+    ));
+
+    commands.spawn((
+        DirectionalLightBundle {
+            directional_light: DirectionalLight {
+                illuminance: 1500.0,
+                shadows_enabled: true,
+                ..default()
+            },
+            transform: Transform::from_xyz(4.0, 8.0, 4.0),
+            ..default()
+        }
+    ));
 
 }
